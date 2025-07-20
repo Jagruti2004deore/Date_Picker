0c319eba45188ba84694940e748c897df1b0c36c
Jagruti2004deore <deorejagruti284@gmail.com>
2025-07-20 21:14:15 +0530

    Wire up RecurringDatePicker in App and style layout

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RecurringDatePicker from './components/RecurringDatePicker';
+
+function App() {
+  return <RecurringDatePicker />;
+}
+
+export default App;
